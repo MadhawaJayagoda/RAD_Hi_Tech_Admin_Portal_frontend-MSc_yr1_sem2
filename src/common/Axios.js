13925558a257f5edaa92b845f47cf02e3af5f15a
MadhawaJayagoda <nipunajayagoda@gmail.com>
2023-05-19 18:42:24 +0530

    Added envs

--- src/common/Axios.js
+++ src/common/Axios.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/v1";
+axios.defaults.baseURL = process.env.BASE_URL;
 axios.interceptors.response.use(
   (resp) => resp,
   async (error) => {
-    console.log("Axios interceptor: Error !!! ", error.response);
     if (error) {
       const response = await axios.post(
         "/token/refresh",
         { userID: "6463e3a13a6e24ffce8a4ae7" },
         { withCredentials: true }
       );
-
       if (response.status === 200) {
         // Update the Authorization header
         axios.defaults.headers.common[
